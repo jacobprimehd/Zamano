650d6e3fd2ee30d9b26044d3139b75fabe3e5204
BuildTools <zamano.jacob@gmail.com>
2020-01-03 16:04:51 -0800

    fix?

--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,6 @@ import '../styles/main.css'
 
 const Home = () => (
   <body className="container mx-auto bg-gray-400 align-middle">
-    <Head></Head>
     <div className="flex justify-center my-32">
       <img src="/image/jacobprime.png" className="rounded-full h-64 w-64 flex items-center justify-center "/>
       <h1>Coming Soon </h1>

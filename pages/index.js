8fce52ab55a46427b07e3d92e91052dffe1b6e2e
BuildTools <zamano.jacob@gmail.com>
2020-01-04 20:11:57 -0800

    centered my work

--- pages/index.js
+++ pages/index.js
@@ -3,11 +3,10 @@ import '../styles/main.css'
 
 
 const Home = () => (
-  <body className="container mx-auto bg-gray-400 align-middle">
-    <div className="flex justify-center my-32">
+  <body className="container mx-auto bg-gray-400">
+    <div className="min-h-screen flex items-center justify-center">
       <img src="/image/jacobprime.png" className="rounded-full h-64 w-64 flex items-center justify-center "/>
-      <h1>Coming Soon </h1>
-      <h1>Shhh... don't tell anyone.</h1>
+      <h1 className="text-6xl">Coming Soon </h1>
     </div>
   </body>
   

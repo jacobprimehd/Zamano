dc7bed5174ee34d8f4f63ad99e0b858a6edca555
BuildTools <zamano.jacob@gmail.com>
2020-01-05 00:59:41 -0800

    code fixes

--- pages/index.js
+++ pages/index.js
@@ -8,9 +8,9 @@ const Home = () => (
         <Head>
             <title>Zamano</title>
         </Head>
-        <div className="min-h-screen flex items-center justify-center">
-            <img src="/image/jacobprime.png" className="rounded-full h-64 w-64 flex items-center justify-center bg-orange-200"/>
-            <h1 className=" ml-6 text-4xl">Coming Soon </h1>
+        <div className="min-h-screen flex flex-col items-center justify-center mb-0">
+            <img src="/image/jacobprime.png" className="rounded-full h-64 w-64 justify-center bg-orange-200"/>
+            <h1 className="text-6xl">Coming Soon </h1>
         </div>
     </body>
 )

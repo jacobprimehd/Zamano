f0f3b634acb562cc506c23d4580fd78abe607325
BuildTools <zamano.jacob@gmail.com>
2020-01-05 00:31:13 -0800

    code fixes + cleaner

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,16 @@
 import React from 'react'
+import HeadPager from '../components/Head.js'
 import '../styles/main.css'
 
 
 const Home = () => (
-  <body className="container mx-auto bg-gray-400">
-    <div className="min-h-screen flex items-center justify-center">
-      <img src="/image/jacobprime.png" className="rounded-full h-64 w-64 flex items-center justify-center "/>
-      <h1 className="text-6xl">Coming Soon </h1>
-    </div>
-  </body>
-  
+    <body className="container mx-auto bg-red-200">
+        <HeadPager/>
+        <div className="min-h-screen flex items-center justify-center">
+            <img src="/image/jacobprime.png" className="rounded-full h-64 w-64 flex items-center justify-center bg-orange-200"/>
+            <h1 className=" ml-6 text-4xl">Coming Soon </h1>
+        </div>
+    </body>
 )
 
 export default Home

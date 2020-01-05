413412f887d79a9281bf8532bd92a1b38971ccd9
BuildTools <zamano.jacob@gmail.com>
2020-01-05 00:46:04 -0800

    code fixes + cleaner

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,13 @@
 import React from 'react'
-import HeadPager from '../components/Head.js'
+import Head from 'next/head'
 import '../styles/main.css'
 
 
 const Home = () => (
     <body className="container mx-auto bg-red-200">
-        <HeadPager/>
+        <Head>
+            <title>Zamano</title>
+        </Head>
         <div className="min-h-screen flex items-center justify-center">
             <img src="/image/jacobprime.png" className="rounded-full h-64 w-64 flex items-center justify-center bg-orange-200"/>
             <h1 className=" ml-6 text-4xl">Coming Soon </h1>

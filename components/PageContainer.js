a278f34fc4331f5f183d7decd89d232fd1b79563
BuildTools <zamano.jacob@gmail.com>
2020-02-29 09:16:17 -0800

    personal pages

--- components/PageContainer.js
+++ components/PageContainer.js
@@ -7,6 +7,8 @@ export default function HomePageContainer(){
         <br/>
         <div className="bg-yellow-200 mt-10">
         <h1 className="mx-auto text-4xl text-black text-center">Part 1</h1>
+        <h1 className="text-center">I am lucky, but not just lucky more than that! I really got to know a beautiful girl from daily phone calls. These phone calls would last more than 3+ hours, talking or even being silent knowing each other is there on the other side.  After a while, our connection changed from what I felt, It went from getting to know each other to a great connection. It first started with flirting then, a sign popped up from Tiktok.
+        </h1>
         </div>
         </div>
     )

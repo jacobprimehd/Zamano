8314176695abbee1c9ad764b0afc192973a65feb
BuildTools <zamano.jacob@gmail.com>
2020-02-29 09:14:46 -0800

    personal pages

--- components/PageContainer.js
+++ components/PageContainer.js
@@ -2,10 +2,12 @@ export default function HomePageContainer(){
     return(
         <div className="container mx-auto bg-white">
         <div className="flex content-center">
-        <h1 className="mx-auto text-5xl text-black">♥ Hello Ramla ♥</h1>
+        <h1 className="mx-auto text-5xl text-black">Hello Ramla♥</h1>
         </div>       
         <br/>
-        <h1>Hello</h1>
+        <div className="bg-yellow-200 mt-10">
+        <h1 className="mx-auto text-4xl text-black text-center">Part 1</h1>
+        </div>
         </div>
     )
 }

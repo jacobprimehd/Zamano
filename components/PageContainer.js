4968e877ce8c540e2a96bdf89478d86df36e9ef1
BuildTools <zamano.jacob@gmail.com>
2020-02-29 09:06:21 -0800

    personal pages

---
+++ components/PageContainer.js
@@ -0,0 +1,11 @@
+export default function HomePageContainer(){
+    return(
+        <div className="container mx-auto bg-white">
+        <div className="flex content-center">
+        <h1 className="mx-auto text-5xl text-black">♥ Hello Ramla ♥</h1>
+        </div>       
+        <br/>
+        <h1>Hello</h1>
+        </div>
+    )
+}
---
+++ pages/foryou.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import Head from 'next/head'
+import PageContainer from '../components/PageContainer'
+import '../styles/main.css'
+
+
+const Story = () => (
+    <body className="container mx-auto bg-red-300">
+
+    <Head>
+        <title>Zamano</title>
+    </Head>
+    <PageContainer></PageContainer>
+    </body>
+)
+
+export default Story

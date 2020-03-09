2555636de67a7b1e83eb4cce046b88dfca8c8ab2
Jacob Zamano <zamano.jacob@gmail.com>
2020-03-08 19:52:48 -0700

    journey.js added

---
+++ pages/journey.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import Head from 'next/head'
+import Navbar from '../components/Navbar'
+import '../styles/main.css'
+
+
+const Story = () => (
+    <body className="mx-auto bg-white">
+
+    <Head>
+        <title>Zamano</title>
+    </Head>
+    <div className="min-h-screen flex flex-col items-center justify-center mb-0">
+        <h1 className="text-6xl underline">The Journey just started!</h1>
+    </div>
+    </body>
+)
+
+export default Story

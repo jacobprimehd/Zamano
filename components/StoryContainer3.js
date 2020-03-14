e13ff6e4539203775482bfc616ccf4e27f386ee2
BuildTools <zamano.jacob@gmail.com>
2020-03-13 23:08:00 -0700

    added ui changes

--- components/StoryContainer3.js
+++ components/StoryContainer3.js
@@ -3,7 +3,7 @@ export default function HomePageContainer(){
         <div className="max-w-md w-full lg:flex" data-aos="fade-up">
         <div className="h-48 lg:h-auto lg:w-48 flex-none bg-cover rounded-t lg:rounded-t-none lg:rounded-l text-center overflow-hidden" title="Woman holding a mug">
         </div>
-        <div className="border-b bg-white rounded-b lg:rounded-b-none lg:rounded-r p-4 flex flex-col justify-between leading-normal">
+        <div className=" bg-white rounded-b lg:rounded-b-none lg:rounded-r p-4 flex flex-col justify-between leading-normal">
           <div className="mb-8">
             <div className="text-black font-bold text-xl mb-2 border-b">Extra</div>
             <p className="text-grey-darker text-base font-mono italic">Cuando mires las estrellas, acuerdate de mi. en cada una de ellas hay un beso para ti.</p>

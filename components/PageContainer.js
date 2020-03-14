4f00a7585c3c0a7cdeeec10fe71f7e011ed74e07
BuildTools <zamano.jacob@gmail.com>
2020-03-13 23:33:04 -0700

    added new file

--- components/PageContainer.js
+++ components/PageContainer.js
@@ -1,6 +1,7 @@
 import Story1 from './StoryContainer'
 import Story2 from './StoryContainer2'
 import Story3 from './StoryContainer3'
+import Story4 from './StoryContainer4'
 import Picture from './PictureContainer'
 import Link from 'next/link'
 
@@ -16,6 +17,7 @@ export default function HomePageContainer(){
         <Story1></Story1>  
         <Story2></Story2>
         <Story3></Story3>
+        <Story4></Story4>
         <Picture></Picture>
         </div>
         </div>
---
+++ components/StoryContainer4.js
@@ -0,0 +1,13 @@
+export default function HomePageContainer(){
+    return(
+        <div className="max-w-md w-full lg:flex" data-aos="zoom-in" data-aos-delay="250">
+        <div className="h-48 lg:h-auto lg:w-48 flex-none bg-cover rounded-t lg:rounded-t-none lg:rounded-l text-center overflow-hidden" title="Woman holding a mug">
+        </div>
+        <div className=" bg-white rounded-b lg:rounded-b-none lg:rounded-r p-4 flex flex-col justify-between leading-normal">
+          <div className="mb-8">
+            <div className="text-black font-bold text-xl mb-2 border-b">Part 3 Coming Soon</div>
+          </div>
+        </div>
+      </div>
+    )
+}

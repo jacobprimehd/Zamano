851149de1f471ef439b43500c9c89a3a168d321f
BuildTools <zamano.jacob@gmail.com>
2020-03-13 23:29:41 -0700

    added ui changes

--- components/PictureContainer.js
+++ components/PictureContainer.js
@@ -1,8 +1,8 @@
 
 export default function HomePageContainer(){
     return(
-        <div class="max-w-sm rounded overflow-hidden">
-        <img class="w-full" src="https://lh3.googleusercontent.com/uG-A-Lw4gq0DPHVt3bLZqMHoKYdsOwGVOs8HnCdPuvJESl8hq1Expw0qe82BUbP1Hm17CDVokLyC_5r5z7X5ZMzIWSkQF3PQTosIRdWSqsjG8vI0gyt6eGwuNmBZnC6TD4o9GZ4AbVvrwPg_iCPHq3n-uh4Cx0ERzCUs3m7jATxUDses378cq9pvhzUwdwGxXYmROH4fsaBx9NYXtJ4n46a3I2G3LYUqamDuvWL-Bf_kqc5nSnWPQ70iWn4JsErIecLw9TjKJW9kAxzWHR3sgoNnIzXqFBAdWqMvDebYs8FKhzD5QExsDR-FtU30SVV7xYBbVJoK6h-MUn5Loj30ITfRR-ROoK_gJ6CN0ncEtb3X-FV3RqIJCUMvSLkGetGgyZ3uoSVC24b2Lld54a3QHisJ3YrWtfOdssJKoFIJbIZgfHD6xEGDebog7r-d3p2A_69oPPEYmmOEt_6mozA_K-oS1CUwWM-f9T_7AqulTdimD6beMy0Oh6t6rKS5AcbDztqcA42ytdzOxfFtBuzRkbavu1vdCoqguAky8J-yUK2GpaET5opPwBlgLynJTogRjyuo5MVYL2GDBw69G4z8nzdxmBPZcT6XqnZlyye88HulE88TANEHqKNDDQPbW6qPRJoJvg-NaTw0LUN6RXUz1OwuWuTv4PoVWwwIDr2Sjmk6aSckc0a7flU=w1707-h1280-no" alt="Sunset in the mountains"/>
+        <div className="max-w-sm rounded overflow-hidden" data-aos="zoom-in" data-aos-delay="250">
+        <img className="w-full" src="https://lh3.googleusercontent.com/uG-A-Lw4gq0DPHVt3bLZqMHoKYdsOwGVOs8HnCdPuvJESl8hq1Expw0qe82BUbP1Hm17CDVokLyC_5r5z7X5ZMzIWSkQF3PQTosIRdWSqsjG8vI0gyt6eGwuNmBZnC6TD4o9GZ4AbVvrwPg_iCPHq3n-uh4Cx0ERzCUs3m7jATxUDses378cq9pvhzUwdwGxXYmROH4fsaBx9NYXtJ4n46a3I2G3LYUqamDuvWL-Bf_kqc5nSnWPQ70iWn4JsErIecLw9TjKJW9kAxzWHR3sgoNnIzXqFBAdWqMvDebYs8FKhzD5QExsDR-FtU30SVV7xYBbVJoK6h-MUn5Loj30ITfRR-ROoK_gJ6CN0ncEtb3X-FV3RqIJCUMvSLkGetGgyZ3uoSVC24b2Lld54a3QHisJ3YrWtfOdssJKoFIJbIZgfHD6xEGDebog7r-d3p2A_69oPPEYmmOEt_6mozA_K-oS1CUwWM-f9T_7AqulTdimD6beMy0Oh6t6rKS5AcbDztqcA42ytdzOxfFtBuzRkbavu1vdCoqguAky8J-yUK2GpaET5opPwBlgLynJTogRjyuo5MVYL2GDBw69G4z8nzdxmBPZcT6XqnZlyye88HulE88TANEHqKNDDQPbW6qPRJoJvg-NaTw0LUN6RXUz1OwuWuTv4PoVWwwIDr2Sjmk6aSckc0a7flU=w1707-h1280-no" alt="Sunset in the mountains"/>
       </div>
     )
 }

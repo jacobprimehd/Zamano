992a4a84981f221f611dd4838b5d0f9883f3637b
BuildTools <zamano.jacob@gmail.com>
2020-03-13 23:11:56 -0700

    added ui changes

--- components/StoryContainer2.js
+++ components/StoryContainer2.js
@@ -3,7 +3,7 @@ export default function HomePageContainer(){
         <div className="max-w-md w-full lg:flex" data-aos="fade-left">
         <div className="h-48 lg:h-auto lg:w-48 flex-none bg-cover rounded-t lg:rounded-t-none lg:rounded-l text-center overflow-hidden" title="Woman holding a mug">
         </div>
-        <div className="border-b bg-white rounded-b lg:rounded-b-none lg:rounded-r p-4 flex flex-col justify-between leading-normal">
+        <div className="bg-white rounded-b lg:rounded-b-none lg:rounded-r p-4 flex flex-col justify-between leading-normal">
           <div className="mb-8">
             <div className="text-black font-bold text-xl mb-2 border-b ">Part 2</div>
             <p className="text-grey-darker text-base font-mono ">Ramla, I am thankful I have a person like you in my life. I have many reasons I can go on and on about it! But there are some things that really pop out to me that I love about you! Ramla you are very patient, and caring. But, I want to say one thing to you Ramla, I am sorry. I failed on what I agreed on, which is definitely not right on my part. I will do my best not to mess up, I hurt you for doing that. I made you bored, and wait for me, which is completely wrong to do on my part. That was mean and cruel of me to do, especially for a person that cares about me. I know what I need to work on. Ramla keeps up the good work in school, you are doing an amazing job! I am really proud of you, I can even express it in words on how amazed I am! xoxo</p>

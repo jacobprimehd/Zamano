8bd120f41215055fc16181dccfdd31cf38a0f664
Jacob Zamano <zamano.jacob@gmail.com>
2020-04-07 20:52:45 -0700

    lol

--- components/StoryContainer4.js
+++ components/StoryContainer4.js
@@ -1,13 +1,21 @@
 export default function HomePageContainer(){
-    return(
-        <div className="max-w-md w-full lg:flex" data-aos="zoom-in" data-aos-delay="250">
-        <div className="h-48 lg:h-auto lg:w-48 flex-none bg-cover rounded-t lg:rounded-t-none lg:rounded-l text-center overflow-hidden" title="Woman holding a mug">
+  return(
+      <div className="max-w-md w-full lg:flex" data-aos="fade-left">
+      <div className="h-48 lg:h-auto lg:w-48 flex-none bg-cover rounded-t lg:rounded-t-none lg:rounded-l text-center overflow-hidden" title="Woman holding a mug">
+      </div>
+      <div className="bg-white rounded-b lg:rounded-b-none lg:rounded-r p-4 flex flex-col justify-between leading-normal">
+        <div className="mb-8">
+          <div className="text-black font-bold text-xl mb-2 border-b ">Part 3</div>
+          <p className="text-grey-darker text-base font-mono ">Ramla, It has been one month, yes you heard me right one month! I want to say one thing, Ramla you are one amazing, kind, and caring individual.  Before I get to say something to you, I would like to say sorry and Thank you honestly! I have not been updating the site very often, pff what am I talking about more like decades. It is something I would like to start doing again, I loved doing it, and you want to read it at least I hope. It put a smile on my face, just writing one of those little journals, but I hope it put a smile on your face, too, including reading my horrible English grammar skills. Lastly, before I head on to the next topic, I would like to discuss what you have received today if you are reading this on the day 04/07/2020, hopefully. The necklace and bracelet are something you deserve, and would like to see you wear! Ramla, you, my lady, and my love have made my days better in many ways, from just being there for me or just being by my side on the phone. From listening to me complaining, acting stupid(More like all the time sadly.), to hopefully being funny! You have done many things to me behind the scenes, and I have personally not shown or told you about it. Honestly, I can go on about so many things you have done personally, which you have not even noticed most likely! You made me cry(being thankful, here for me, and just making me happy like in the car ride in the morning.), be happy, and laugh to being from the phone calls we had at the very start of knowing each other to now facetime. Me always, admiring you 24/7 when we are the call to you slapping me through the phone.  Ramla, as you sent me a text at 1 am "Happy one month babe!! Many more to come." yes, darling many more to come <3.
+          </p>
         </div>
-        <div className=" bg-white rounded-b lg:rounded-b-none lg:rounded-r p-4 flex flex-col justify-between leading-normal">
-          <div className="mb-8">
-            <div className="text-black font-bold text-xl mb-2 border-b">Part 3 Coming Soon</div>
+        <div className="flex items-center">
+          <div className="text-sm">
+          <p className="text-black leading-none">Sincerely,</p>
+          <p className="text-black leading-none">Jacob Zamano</p>
           </div>
         </div>
       </div>
-    )
+    </div>
+  )
 }

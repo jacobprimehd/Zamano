77965935ce9e416e86727b34c415ae542045a5cc
Jacob Zamano <jacobzamano@pop-os.localdomain>
2020-07-20 00:25:36 -0700

    adding a happy birthday message <3"

--- components/Ramla/Picture.js
+++ components/Ramla/Picture.js
@@ -4,7 +4,7 @@ export default function HomePageContainer(){
         <div className="max-w-sm rounded overflow-hidden">
         <img className="w-full" data-aos="zoom-in" data-aos-delay="250" src="./image/IMG_0427.jpg" alt=""/>
         <img className="w-full" data-aos="zoom-in" data-aos-delay="250" src="./image/IMG_0785.jpg" alt=""/>
-        <img className="w-full" data-aos="zoom-in" data-aos-delay="250" src="./image/IMG_1226.jpg" alt=""/>
+        <img className="w-full" data-aos="zoom-in" data-aos-delay="250" src="./image/IMG_1226.png" alt=""/>
         <img className="w-full" data-aos="zoom-in" data-aos-delay="250" src="./image/IMG_1829.jpg" alt=""/>
         <img className="w-full" data-aos="zoom-in" data-aos-delay="250" src="./image/moments_06_20_20_50_57.jpg" alt=""/>
       </div>
